663065b33341b66b383ae7a378d8ac417b013188
João Lucas Abreu <joaolucasabreunogueira@gmail.com>
2025-08-13 22:53:20 -0300

    first commit

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+import RotatingDotsProfile from "@/components/rotatingDotsProfile";
+
+export default function Home() {
+    return (
+        <main className="bg-[#121212] h-screen flex items-start justify-center pt-20">
+            <RotatingDotsProfile />
+        </main>
+    );
+}
